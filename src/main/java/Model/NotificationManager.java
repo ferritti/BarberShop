cf85028a67aa9ce3361277a19a7a32c351db3802
Andrea Ferritti <126817248+ferritti@users.noreply.github.com>
2025-03-02 17:01:57 +0100

    Added NotificationManager implementation

--- src/main/java/Model/NotificationManager.java
+++ src/main/java/Model/NotificationManager.java
@@ -1,4 +1,28 @@
 package Model;
+import java.util.ArrayList;
+import java.util.List;
 
-public class NotificationManager {
+public class NotificationManager implements NotificationSubject {
+    private List<NotificationObserver> observers = new ArrayList<>();
+
+    @Override
+    public void addObserver(NotificationObserver observer) {
+        observers.add(observer);
+    }
+
+    @Override
+    public void removeObserver(NotificationObserver observer) {
+        observers.remove(observer);
+    }
+
+    @Override
+    public void notifyObservers(Notification notification) {
+        for (NotificationObserver observer : observers) {
+            if (notification.getTargetType() == Notification.TargetType.ALL ||
+                    (notification.getTargetType() == Notification.TargetType.BARBER && observer.getUserType() == User.UserType.BARBER) ||
+                    (notification.getTargetType() == Notification.TargetType.CUSTOMER && observer.getUserType() == User.UserType.CUSTOMER)) {
+                observer.update(notification);
+            }
+        }
+    }
 }
--- src/main/java/Model/NotificationSubject.java
+++ src/main/java/Model/NotificationSubject.java
@@ -1,8 +1,8 @@
 package Model;
 
 public interface NotificationSubject {
-    public void addObserver(NotificationObserver no);
-    public void removeObserver(NotificationObserver no);
-    public void notifyObserver(Notification notification);
+    public void addObserver(NotificationObserver notificationObs);
+    public void removeObserver(NotificationObserver notificationObs);
+    public void notifyObservers(Notification notification);
 
 }
