2cd1a9111874d62f54f66da0332e6c79a641d827
Andrea Ferritti <126817248+ferritti@users.noreply.github.com>
2025-03-02 16:13:47 +0100

    Added Notification implementation

--- src/main/java/Model/Notification.java
+++ src/main/java/Model/Notification.java
@@ -1,4 +1,38 @@
 package Model;
 
 public class Notification {
+    private String title;
+    private String message;
+    private final TargetType targetType;
+
+    public static enum TargetType {
+        CUSTOMER, BARBER, ALL;
+    }
+
+    public Notification(String title, String message, TargetType targetType) {
+        this.title = title;
+        this.message = message;
+        this.targetType = targetType;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public TargetType getTargetType() {
+        return targetType;
+    }
 }
+
--- src/main/java/Model/ServiceType.java
+++ src/main/java/Model/ServiceType.java
@@ -30,5 +30,3 @@ public class ServiceType {
         this.price = price;
     }
 }
-
-
