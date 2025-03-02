b0dc68f1633594753476caa3cbb58d14dfea3d60
Jieke Lin <Jieke.lin@edu.unifi.it>
2025-03-02 18:17:13 +0100

    Added DBManager implementation

---
+++ src/main/java/DBconnection/Database/DBManager.java
@@ -0,0 +1,52 @@
+package DBconnection.Database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBManager {
+    public static DBManager manager = null;
+    private Connection connection = null;
+
+    public static final String URL = "jdbc:postgresql://localhost:5432/BarberShopDB";
+    public static final String User = "userSWE";
+    public static final String Password = "swepass";
+
+    private DBManager() {
+        try {
+            this.connection = DriverManager.getConnection(URL, User, Password);
+            System.out.println("Connection established");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Connection failed");
+        }
+    }
+
+    private static DBManager init() {
+        if (manager == null) {
+            manager = new DBManager();
+        }
+        return manager;
+    }
+
+    public static DBManager getInstance() {
+        return init();
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void close() {
+        try {
+            if (manager != null && manager.connection != null && !manager.connection.isClosed()) {
+                manager.connection.close();
+                System.out.println("Connection closed");
+                manager = null;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
--- src/main/java/DBconnection/Database/DBmanager.java
+++
@@ -1,4 +0,0 @@
-package DBconnection.Database;
-
-public class DBmanager {
-}
