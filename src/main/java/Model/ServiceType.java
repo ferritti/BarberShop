fdb7b3d519172e7efe6e6951feee5f991e6e58e0
Andrea Ferritti <126817248+ferritti@users.noreply.github.com>
2025-03-02 15:38:56 +0100

    Added ServiceType implementation

--- src/main/java/Model/ServiceType.java
+++ src/main/java/Model/ServiceType.java
@@ -1,4 +1,34 @@
 package Model;
 
 public class ServiceType {
+    private String nameService;
+    private double price;
+
+    public ServiceType() {
+        this.nameService = "";
+        this.price = 0.0;
+    }
+
+    public ServiceType(String nameService, double price) {
+        this.nameService = nameService;
+        this.price = price;
+    }
+
+    public String getNameService() {
+        return nameService;
+    }
+
+    public void setNameService(String nameService) {
+        this.nameService = nameService;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
+
+
