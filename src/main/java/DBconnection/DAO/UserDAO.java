5c56b20f505ce15d20f69312f128a46f566e1cd3
Jieke Lin <Jieke.lin@edu.unifi.it>
2025-03-02 18:33:36 +0100

    Added UserDAO implementation

--- src/main/java/DBconnection/DAO/UserDAO.java
+++ src/main/java/DBconnection/DAO/UserDAO.java
@@ -1,4 +1,10 @@
 package DBconnection.DAO;
+import Model.User;
+
+import java.sql.SQLException;
 
 public interface UserDAO {
+    public void addUser(User user) throws SQLException;
+    public void removeUser(User user) throws SQLException;
+    public User findByEmail(String email) throws SQLException;
 }
