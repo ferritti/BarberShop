e318b1a362c407a9fdfa6994ea75c7a190f6b46c
Jieke Lin <Jieke.lin@edu.unifi.it>
2025-03-02 16:44:42 +0100

    Added Customer implementation

--- src/main/java/Model/Customer.java
+++ src/main/java/Model/Customer.java
@@ -1,4 +1,8 @@
 package Model;
 
-public class Customer {
+public class Customer extends User{
+
+    public Customer(int idUser, String name, String surname, String email, String password, String phone) {
+        super(idUser, name, surname, email, password, phone, UserType.CUSTOMER);
+    }
 }
