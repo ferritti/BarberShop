cd27b969719f5037f4267923aecae6e447ded71f
Filippo Sansavini <filippo.sansavini@edu.unifi.it>
2025-03-02 16:46:57 +0100

    Added Barber implementation

--- src/main/java/Model/Barber.java
+++ src/main/java/Model/Barber.java
@@ -1,4 +1,7 @@
 package Model;
 
-public class Barber {
+public class Barber extends User {
+    public Barber(int idUser, String name, String surname, String email, String password, String phone) {
+        super(idUser, name, surname, email, password, phone, UserType.BARBER);
+    }
 }
