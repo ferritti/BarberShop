b5cd148c6a03b2903f4cbabb2a6c522edfe58aca
Filippo Sansavini <filippo.sansavini@edu.unifi.it>
2025-03-02 18:54:48 +0100

    Added AppointmentDAO implementations

--- src/main/java/DBconnection/DAO/AppointmentDAO.java
+++ src/main/java/DBconnection/DAO/AppointmentDAO.java
@@ -1,4 +1,10 @@
 package DBconnection.DAO;
+import Model.Appointment;
+import java.sql.SQLException;
+import java.util.List;
 
 public interface AppointmentDAO {
+    public void addAppointment(Appointment appointment) throws SQLException;
+    public void removeAppointment(Appointment appointment) throws SQLException;
+    public List<Appointment> findByEmailOfUser(String email) throws SQLException;
 }
