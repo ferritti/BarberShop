b659b896ab861affab7038c9ecb2dc12a1d8506f
Andrea Ferritti <126817248+ferritti@users.noreply.github.com>
2025-03-02 18:54:07 +0100

    Added AvailableSlotsDAO and ServiceTypeDAO implementations

--- src/main/java/DBconnection/DAO/AvailableSlotsDAO.java
+++ src/main/java/DBconnection/DAO/AvailableSlotsDAO.java
@@ -1,4 +1,12 @@
 package DBconnection.DAO;
 
+import Model.Appointment;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.List;
+
 public interface AvailableSlotsDAO {
+    public void addAvSlot(Appointment appointment) throws SQLException;
+    public void removeAvSlot(Appointment appointment) throws SQLException;
+    public List<Appointment> getAvSlotsAtSelectedDate(LocalDate Date) throws SQLException;
 }
--- src/main/java/DBconnection/DAO/ServiceTypeDAO.java
+++ src/main/java/DBconnection/DAO/ServiceTypeDAO.java
@@ -1,4 +1,13 @@
 package DBconnection.DAO;
 
+import Model.ServiceType;
+
+import java.sql.SQLException;
+
+import java.util.List;
+
 public interface ServiceTypeDAO {
+    public void addServiceType(ServiceType serviceType) throws SQLException;
+    public void removeServiceType(ServiceType serviceType) throws SQLException;
+    public List<ServiceType> getAllServiceTypes() throws SQLException;
 }
