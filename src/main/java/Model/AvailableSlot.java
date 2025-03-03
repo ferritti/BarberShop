92c585ac8921aeac365c3c79ac5937e86e4c9b3a
Andrea Ferritti <126817248+ferritti@users.noreply.github.com>
2025-03-03 16:52:52 +0100

    Added AvailableSlot class in the Model package

---
+++ src/main/java/Model/AvailableSlot.java
@@ -0,0 +1,49 @@
+package Model;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class AvailableSlot {
+    private String barberEmail;
+    private LocalDate date;
+    private LocalTime startTime;
+    private LocalTime endTime;
+
+    public AvailableSlot(String barberEmail, LocalDate date, LocalTime startTime, LocalTime endTime) {
+        this.barberEmail = barberEmail;
+        this.date = date;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public String getBarberEmail() {
+        return barberEmail;
+    }
+
+    public void setBarberEmail(String barberEmail) {
+        this.barberEmail = barberEmail;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+    public void setStartTime(LocalTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalTime endTime) {
+        this.endTime = endTime;
+    }
+}
