cca7828b4a71e09209c1731b5d4fc0fc27280194
Andrea Ferritti <126817248+ferritti@users.noreply.github.com>
2025-03-07 22:46:29 +0100

    Added HashingPasswordService implementantion and related dependency in pom.xml

--- src/main/java/Business/HashingPasswordService.java
+++ src/main/java/Business/HashingPasswordService.java
@@ -1,6 +1,16 @@
 package Business;
 
+import org.mindrot.jbcrypt.BCrypt;
+
 public class HashingPasswordService {
-    public static String hashPassword(String password) {}
-    public static boolean checkPassword(String password, String hashedPass) {}
+
+        public static String hashPassword(String password) {
+            String salt = BCrypt.gensalt();
+            return BCrypt.hashpw(password, salt);
+        }
+
+        public static boolean checkPassword(String password, String hashedPassword) {
+            return BCrypt.checkpw(password, hashedPassword);
+    }
+
 }
