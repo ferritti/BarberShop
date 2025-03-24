3ac14c9fc66ccf40481f8042a71a96f2cd7ea22a
Andrea Ferritti <126817248+ferritti@users.noreply.github.com>
2025-03-24 17:41:16 +0100

    Improved SessionManager

--- src/main/java/Authentication/SessionManager.java
+++ src/main/java/Authentication/SessionManager.java
@@ -5,6 +5,7 @@ import Model.User;
 public class SessionManager {
     private static SessionManager instance;
     private User currentUser;
+
     private SessionManager() {}
 
     public static SessionManager getInstance() {
@@ -15,15 +16,24 @@ public class SessionManager {
     }
 
     public void setCurrentUser(User user) {
-        currentUser = user;
+        if (user == null) {
+            throw new IllegalArgumentException("User cannot be null");
+        }
+        this.currentUser = user;
     }
 
     public User getCurrentUser() {
+        if (currentUser == null) {
+            throw new IllegalStateException("User not logged in");
+        }
         return currentUser;
+    }
+
+    public void resetUser() {
+        currentUser = null;
     }
 
     public void closeSession() {
         instance = null;
     }
-
 }
